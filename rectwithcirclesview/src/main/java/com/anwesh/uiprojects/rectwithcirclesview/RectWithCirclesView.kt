16e44be1045cad16c03c801bd232fbf998958656
Anwesh43 <anweshthecool0@gmail.com>
2019-03-04 19:45:09 +0530

    adding RectWithCircles class

--- rectwithcirclesview/src/main/java/com/anwesh/uiprojects/rectwithcirclesview/RectWithCirclesView.kt
+++ rectwithcirclesview/src/main/java/com/anwesh/uiprojects/rectwithcirclesview/RectWithCirclesView.kt
@@ -164,4 +164,27 @@ class RectWithCirclesView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RectWithCircles(var i : Int) {
+        private val root : RWCNode = RWCNode(0)
+        private var curr : RWCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
