207acc50660d9331dc5b5e98b52662f551ad02b3
Anwesh43 <anweshthecool0@gmail.com>
2019-03-04 17:29:43 +0530

    adding constants and imporitng libraries

--- rectwithcirclesview/src/main/java/com/anwesh/uiprojects/rectwithcirclesview/RectWithCirclesView.kt
+++ rectwithcirclesview/src/main/java/com/anwesh/uiprojects/rectwithcirclesview/RectWithCirclesView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.rectwithcirclesview
 /**
  * Created by anweshmishra on 04/03/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val circles : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
