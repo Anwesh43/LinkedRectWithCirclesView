03358e0e0bdc01142a60bdf5858a5bdbf04cc3e1
Anwesh43 <anweshthecool0@gmail.com>
2019-03-04 20:19:16 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectwithcirclesview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectwithcirclesview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrectwithcirclesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectwithcirclesview.RectWithCirclesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectWithCirclesView.create(this)
     }
 }
